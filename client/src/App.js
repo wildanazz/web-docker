4363e815fc4e68ea025f9021be821e93a57e9d10
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-02-22 12:55:18 +1000

    Edit app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hi there! my personal web will be available soon</p>
       </header>
     </div>
   );

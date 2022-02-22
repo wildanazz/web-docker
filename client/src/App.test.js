ed1d3eabd3b697f423b186127d2c2d13f0c026c8
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-02-22 13:02:46 +1000

    Edit test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders message", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /Hi there! my personal web will be available soon/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
